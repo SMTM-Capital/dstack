25c519126571749d9ce283ce7f00993feba765e7
Bihan  Rana <bihan@Bihans-MacBook-Pro.local>
2024-02-21 06:38:59 +0545

    Added Offer Adjustment Comments

--- src/dstack/_internal/core/backends/base/offers.py
+++ src/dstack/_internal/core/backends/base/offers.py
@@ -2,8 +2,8 @@ from dataclasses import asdict
 from typing import Callable, List, Optional
 
 import gpuhunt
-from dstack._internal.core.backends.cudocompute.utils import get_cudo_pricing
 
+from dstack._internal.core.backends.cudocompute.utils import get_cudo_pricing
 from dstack._internal.core.models.backends.base import BackendType
 from dstack._internal.core.models.instances import (
     Disk,
@@ -25,10 +25,10 @@ def get_catalog_offers(
     provider = backend.value
     if backend == BackendType.LAMBDA:
         provider = "lambdalabs"
+    # Todo discuss disk_size_calculation
     elif backend == BackendType.CUDOCOMPUTE:
         provider = "cudocompute"
         if requirements is not None and requirements.resources.disk is not None:
-            disk_size_min = requirements.resources.disk.size.min
             disk_size_max = requirements.resources.disk.size.max
             requirements.resources.disk.size.min = 1
             requirements.resources.disk.size.max = 1
@@ -40,6 +40,7 @@ def get_catalog_offers(
     for item in catalog.query(**asdict(q)):
         if locations is not None and item.location not in locations:
             continue
+        # Todo discuss disk_size_calculation
         if backend == BackendType.CUDOCOMPUTE and requirements.resources.disk is not None:
             item.disk_size = disk_size_max
         offer = catalog_item_to_offer(backend, item, requirements)
@@ -64,6 +65,7 @@ def catalog_item_to_offer(
     )
     if backend == BackendType.CUDOCOMPUTE:
         if requirements.resources.disk is not None:
+            # Todo discuss disk_size_calculation
             updated_price = get_cudo_pricing(item.price, item.disk_size)
             disk_size_mib = round(
                 item.disk_size * requirements.resources.disk.size.min * 1024
@@ -73,7 +75,7 @@ def catalog_item_to_offer(
                 else 102400  # TODO: Make requirements' fields required
             )
         else:
-            updated_price=item.price
+            updated_price = item.price
 
         resources = Resources(
             cpus=item.cpu,
--- src/dstack/_internal/core/backends/cudocompute/utils.py
+++ src/dstack/_internal/core/backends/cudocompute/utils.py
@@ -1,6 +1,6 @@
-def get_cudo_pricing(total_price,disk_size)->float:
+def get_cudo_pricing(total_price, disk_size) -> float:
     if disk_size is None:
-        disk_size=1
-    price_per_disk=float(0.00012)
-    updated_price=total_price-price_per_disk+(price_per_disk*disk_size)
+        disk_size = 1
+    disk_price_per_gb_per_hr = float(0.000107)  # Todo rate depends on region
+    updated_price = total_price - disk_price_per_gb_per_hr + (disk_price_per_gb_per_hr * disk_size)
     return updated_price
